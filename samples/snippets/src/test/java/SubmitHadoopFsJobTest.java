b1beeef13c1bb6a0d64798024bf07ea6c742479a
Lo Ferris <loferris@google.com>
2022-03-28 11:53:00 -0700

    fixing imports

--- samples/snippets/src/test/java/SubmitHadoopFsJobTest.java
+++ samples/snippets/src/test/java/SubmitHadoopFsJobTest.java
@@ -17,10 +17,12 @@
 import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.google.api.gax.longrunning.OperationFuture;
 import com.google.cloud.dataproc.v1.Cluster;
+import com.google.cloud.dataproc.v1.ClusterConfig;
 import com.google.cloud.dataproc.v1.ClusterControllerClient;
 import com.google.cloud.dataproc.v1.ClusterControllerSettings;
-import com.google.cloud.dataproc.v1.ClusterConfig;
+import com.google.cloud.dataproc.v1.ClusterOperationMetadata;
 import com.google.cloud.dataproc.v1.InstanceGroupConfig;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
